00f22ea7b96d83e02c44a8d1e6e6f07c20cb5f3f
VoxanyNet <contact@voxany.net>
2024-07-25 18:33:29 -0400

    read last 10 posts from firestore

---
+++ app/src/main/java/bestteam/bestrecipeapp/NewPost.java
@@ -0,0 +1,13 @@
+package bestteam.bestrecipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
