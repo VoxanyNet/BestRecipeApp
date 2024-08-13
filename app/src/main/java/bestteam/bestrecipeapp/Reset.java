00cc9ff0bde1275b61ea901925ff0a7a6bfdeec4
James Howell <JEHOWELL@student.fullsail.edu>
2024-08-13 14:24:37 -0500

    password reset UI

---
+++ app/src/main/java/bestteam/bestrecipeapp/Reset.java
@@ -0,0 +1,13 @@
+package bestteam.bestrecipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset);
+    }
+}
