9fb99b052683c13f0e14fda3b68f1da4221ee02e
T7love <t7loveridge@gmail.com>
2024-08-26 15:15:47 -0600

    Functionality for Favorites Button on home page as well as for the back button on Favorites Page

--- app/src/main/java/bestteam/bestrecipeapp/FavoritesActivity.java
+++ app/src/main/java/bestteam/bestrecipeapp/FavoritesActivity.java
@@ -1,6 +1,30 @@
 package bestteam.bestrecipeapp;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class FavoritesActivity extends Activity {
+public class FavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorites_list);
+
+        // Set OnClickListener for the back button
+        ImageButton btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate back to the HomeActivity
+                Intent intent = new Intent(FavoritesActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/bestteam/bestrecipeapp/Home.java
+++ app/src/main/java/bestteam/bestrecipeapp/Home.java
@@ -1,13 +1,25 @@
 package bestteam.bestrecipeapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.*;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -20,66 +32,71 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        setContentView(R.layout.activity_home);
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        List<RecipePost> recipes = new ArrayList<RecipePost>();
+        List<RecipePost> recipes = new ArrayList<>();
 
-        // create a query for the database to return the last 10 created recipes
+        // Create a query for the database to return the last 10 created recipes
         Query recipesQuery = db.collection("recipes").limit(10);
 
-        // execute the query
+        // Execute the query
         Task<QuerySnapshot> response = recipesQuery.get();
 
-        // add on complete listener
+        // Add on complete listener
         response.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-
             @Override
             public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
 
-                    // assemble recipe models from response
+                    // Assemble recipe models from response
                     for (QueryDocumentSnapshot document : response.getResult()) {
-
                         recipes.add(
-                            new RecipePost(
-                                document.get("title").toString(),
-                                document.get("author").toString(),
-                                document.get("description").toString()
-                            )
+                                new RecipePost(
+                                        document.getString("title"),
+                                        document.getString("author"),
+                                        document.getString("description")
+                                )
                         );
                     }
 
-                    // actually create the fragment
-                    ScrollableRecipePostsFragment fragment = new ScrollableRecipePostsFragment(recipes);
+                    // Find the recent_posts_list container
+                    LinearLayout recentPostsList = findViewById(R.id.recent_posts_list);
 
-                    // add the fragment to the activity
-                    getSupportFragmentManager().beginTransaction()
-                            .add(R.id.fragmentContainerView, fragment)
-                            .commit();
+                    // Inflate post.xml for each RecipePost and add it to the container
+                    LayoutInflater inflater = LayoutInflater.from(Home.this);
 
-                }
+                    for (RecipePost recipe : recipes) {
+                        View postView = inflater.inflate(R.layout.post, recentPostsList, false);
 
-                else {
-                    Log.println(Log.ERROR, "test", "Fail");
+                        // Set the content of the inflated view
+                        TextView title = postView.findViewById(R.id.recipePostTitle);
+                        TextView author = postView.findViewById(R.id.recipePostAuthor);
+                        TextView description = postView.findViewById(R.id.recipePostDescription);
 
+                        title.setText(recipe.title);
+                        author.setText(recipe.author);
+                        description.setText(recipe.description);
+
+                        // Add the view to the recent_posts_list container
+                        recentPostsList.addView(postView);
+                    }
+
+                } else {
+                    Log.e("Home", "Failed to fetch recipes");
                     Toast.makeText(Home.this, "Failed to fetch recipes", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-
-
-//        for (int i = 0; i < 100; i++) {
-//            recipes.add(
-//                    new RecipePost(
-//                            "Example Title",
-//                            "Author",
-//                            "Description"
-//                    )
-//            );
-//        }
-
+        // Set OnClickListener for the bottom_btn_favorite button
+        ImageButton btnFavorite = findViewById(R.id.bottom_btn_favorite);
+        btnFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the FavoritesActivity when the button is clicked
+                Intent intent = new Intent(Home.this, FavoritesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
